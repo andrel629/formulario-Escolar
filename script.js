1b35fcad6a796970618a4d6d5bc0e96b66047e03
andrel629 <andrelucas629@gmail.com>
2026-06-08 20:00:09 -0300

    HelloWorld

---
+++ script.js
@@ -0,0 +1,51 @@
+const form=document.getElementById("myForm")
+    
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        Formulario();
+    })
+
+function login() {
+    const inputname=document.getElementById("inputname").value
+    const inputsenha=document.getElementById("inputsenha").value
+  
+   
+
+    if (inputname==="" && inputsenha==="") {
+        alert("Error")
+    } else if(inputsenha.length<3) {
+        alert("Senha pequena")
+    }else if (inputname=="adm@gmail.com" && inputsenha=="123"){
+       alert("Aqui")
+        window.location.href='Formulario.html'
+        return
+    }else{
+        alert("Adoletinha")
+    }
+}
+
+    
+
+function Formulario() {
+    const nome=document.getElementById("nomeAluno")
+    const sobrenome=document.getElementById("sobrenomeAluno")
+    const email=document.getElementById("emailAluno")
+    const number=document.getElementById("idadeAluno")
+
+
+
+
+if(nome===""||sobrenome===""||email===""||number===""){
+    window.location.href('https://www.youtube.com/watch?v=H-v4oZ6q8o0')
+    console.log("Aqui 22")
+}
+if(!sexo){
+    alert("PAPA")
+    return
+}
+
+    const escolaridade=document.querySelector("escolaridade").value
+    const sexo=document.querySelector("input[name='sexo']:checked").value
+    const turno=document.querySelectorAll("input[name=turno]:checked")
+}
+
